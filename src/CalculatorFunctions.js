db852e8859532d148b40cd35f83cf146ab3bed61
Randy Thomas <rlthomasmaster@RLs-MacBook-Pro.local>
2018-07-16 17:32:29 -0700

    JAVASCRIPT CODED AND HTML SETUP

---
+++ src/CalculatorFunctions.js
@@ -0,0 +1,26 @@
+
+
+//The following functions are for Multiplication, Division, Addition, and Subtraction.
+
+//These functions are only available to handle 2 numbers at a time.
+
+function addition(num1, num2){
+  return num1 + num2
+};
+
+function subtraction(num1, num2){
+  return num1 - num2
+};
+
+function multiplication(num1, num2){
+  return num1 * num2
+};
+
+function division(num1, num2){
+  return num1 / num2
+};
+
+var add = console.log(addition());
+var subtract = console.log(subtraction());
+var multiply = console.log(multiplication());
+var divide = console.log(division());
